a7d2b26808b9edf5ce5cbc4fc061fb1a54f2573c
HARSHA VARDHAN V <prithikang3108@gmail.com>
2022-10-08 21:33:14 +0530

    testing static

--- congential/settings.py
+++ congential/settings.py
@@ -112,6 +112,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 
 CSRF_TRUSTED_ORIGINS = ['https://jdf.azurewebsites.net/']
 

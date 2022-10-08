ad24e9d82d2322c86eee07c78b7526ace8cfe646
HARSHA VARDHAN V <vsandeepvvr@gmail.com>
2022-10-08 20:41:55 +0530

    model added unit testing incomplete

--- imtopf/models.py
+++ imtopf/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class user_pdf(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_image = models.ImageField(upload_to='user_images')
+    user_pdf = models.FileField(upload_to='user_pdfs')

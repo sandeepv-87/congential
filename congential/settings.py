8d1f8039df22c2b3bd954732a8c138c8beea16c6
HARSHA VARDHAN V <uec20136@rmd.ac.in>
2022-10-08 21:34:44 +0530

    testtt static

--- congential/settings.py
+++ congential/settings.py
@@ -113,11 +113,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'imtopf/static')
 ]
+
 
 CSRF_TRUSTED_ORIGINS = ['https://jdf.azurewebsites.net/']
 
